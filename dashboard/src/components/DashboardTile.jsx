076572320df78562ecbfa7dd7bb9c90b8b72f06b
m4rvin42 <m4rvin42@hotmail.de>
2025-07-02 17:12:29 +0200

    Add alignment props to DashboardTile

--- dashboard/src/components/DashboardTile.jsx
+++ dashboard/src/components/DashboardTile.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 
-export default function DashboardTile({ children }) {
-  return <div className="dashboard-tile">{children}</div>
+const verticalMap = {
+  top: 'flex-start',
+  center: 'center',
+  bottom: 'flex-end',
+}
+
+const horizontalMap = {
+  left: 'flex-start',
+  center: 'center',
+  right: 'flex-end',
+}
+
+export default function DashboardTile({
+  children,
+  verticalContentAlignment,
+  horizontalContentAlignment,
+}) {
+  const style = {}
+  if (verticalContentAlignment) {
+    style.justifyContent = verticalMap[verticalContentAlignment]
+  }
+  if (horizontalContentAlignment) {
+    style.alignItems = horizontalMap[horizontalContentAlignment]
+  }
+
+  return (
+    <div className="dashboard-tile" style={style}>
+      {children}
+    </div>
+  )
 }
 
