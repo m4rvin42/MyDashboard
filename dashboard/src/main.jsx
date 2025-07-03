bd02a03fef449a65880f0262077e1336dcdb9f34
m4rvin42 <m4rvin42@hotmail.de>
2025-07-03 23:26:42 +0200

    Add debug backgrounds

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './debug.css'
 import App from './App.jsx'
+
+const isDebug = import.meta.env.VITE_DEBUG === 'true'
+if (isDebug && typeof document !== 'undefined') {
+  document.body.classList.add('debug')
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
