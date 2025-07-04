81221a7d45371dcc05b41a5a39a9a2c2f1d22349
m4rvin42 <m4rvin42@hotmail.de>
2025-07-04 21:55:23 +0200

    Add DateTime and String widgets

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import TestWidget from './TestWidget.jsx'
+import DateTimeWidget from './DateTimeWidget.jsx'
+import StringWidget from './StringWidget.jsx'
 import { VerticalStackPanel, HorizontalStackPanel } from './StackPanels.jsx'
 
 function App() {
   return (
     <VerticalStackPanel>
       <HorizontalStackPanel style={{ flex: 1 }}>
-        <TestWidget />
-        <TestWidget />
+        <DateTimeWidget />
+        <StringWidget text="Hello from StringWidget" />
       </HorizontalStackPanel>
       <HorizontalStackPanel style={{ flex: 1 }}>
         <TestWidget />
