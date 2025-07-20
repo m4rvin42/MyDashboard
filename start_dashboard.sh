81e69cb8f9fe7050cd5ed3f3d9a6fba1da448920
Jan Bahlmann <m4rvin42@hotmail.de>
2025-07-20 14:16:41 +0200

    adjusted scripts

--- start_dashboard.sh
+++ start_dashboard.sh
@@ -2,6 +2,6 @@
 # Start the compose stack with PUBLIC_IP env variable
 set -e
 # Obtain public IP
-PUBLIC_IP= 192.168.2.35 #$(curl -s https://api.ipify.org)
+PUBLIC_IP=192.168.2.35 #$(curl -s https://api.ipify.org)
 export PUBLIC_IP
 exec docker compose up
